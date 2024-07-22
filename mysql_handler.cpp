277223ae5fdd11132e872c7db5c4b6019b4a10ec
Michael Sekora <27209981+MichaelSekora@users.noreply.github.com>
2024-07-22 08:19:24 +0200

    Update mysql_handler.cpp

--- mysql_handler.cpp
+++ mysql_handler.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string>
 
+extern bool use_database;
 bool mysqlpresent = false;
 MYSQL *conn = nullptr;
 int version;
@@ -13,11 +14,19 @@ extern std::string dbpasswd;
 
 MYSQL_RES *mysql_perform_query(MYSQL *connection, char *sql_query)
 {
-  // send the query to the database
   if (mysql_query(connection, sql_query))
   {
-    printf("MySQL query error : %s\ncheck MySQL configuration in sipcollect.config\n  dbhost:%s\n  dbname:%s\n  dbtablename:sip\n  dbuser:%s\n  dbpasswd:********\n",
+    printf("\n/// START ////////////////////////////////////////////////////");
+    printf("\nMySQL query error : %s\ncheck MySQL configuration in sipcollect.config\n  dbhost:%s\n  dbname:%s\n  dbtablename:sip\n  dbuser:%s\n  dbpasswd:********\n",
            mysql_error(connection), dbhost.c_str(), dbname.c_str(), dbuser.c_str());
+    printf("\nquery:%s", sql_query);
+    printf("\n///////////////////////////////////////////////////////");
+    for(int i = 0; i < 2000; i++)
+    {
+      printf("%02x ", sql_query[i]);
+    }
+    printf("\n/// END ////////////////////////////////////////////////////");
+    
   }
   return mysql_use_result(connection);
 }
@@ -27,8 +36,11 @@ int connectdb()
   conn = nullptr;
   if (!mysqlpresent)
   {
+    printf("\ntrying to connect to Database\n");
     conn = mysql_init(NULL);
+    mysql_set_character_set(conn, "utf8mb4");
     mysql_real_connect(conn, dbhost.c_str(), dbuser.c_str(), dbpasswd.c_str(), dbname.c_str(), 3306, NULL, 0);
+
     version = mysql_get_server_version(conn);
     printf("\nMySQL Version = %d\n", version);
     mysqlpresent = true;
